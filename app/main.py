0837076b2235274887f9822a41fdb0a60dac14b8
Hanan Bahtiti <55956102+hananbahtiti@users.noreply.github.com>
2025-03-14 14:01:35 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -32,16 +32,10 @@ async def generate_image(prompt: str):
   """Add an image generation request to the queue"""
   if not prompt:
     raise HTTPException(status_code=400, detail="Prompt cannot be empty")
-  job = queue.enqueue(tasks.generate_image, prompt)
+  job = queue.enqueue(tasks.generate_image, prompt, client_id)
   return {"job_id": job.id, "message": "Request added to queue" }
 
 
-@app.get("/status/{job_id}")
-async def check_status(job_id: str):
-  """Check the status of a queued job"""
-  job = queue.fetch_job(job_id)
-  if job in None:
-    raise HTTPExcetion(status_code=404, detail="Job not found")
-  return{"job_id": job.id, "status": job.get_status(), "result": job.result}
+
     
 
