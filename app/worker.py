bb45265e0e7193694dca24810aea09b199c6e891
Hanan Bahtiti <55956102+hananbahtiti@users.noreply.github.com>
2025-03-26 14:41:26 +0300

    Update worker.py

--- app/worker.py
+++ app/worker.py
@@ -1,5 +1,8 @@
 from redis import Redis
 from rq import Worker, Queue, Connection
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 # Connect to Redis
 redis_conn = Redis(host="redis", port=6379)
@@ -10,4 +13,5 @@ queue_name = "image_requests"
 if __name__ == "__main__":
     with Connection(redis_conn):
         worker = Worker([queue_name])
+        logging.info("Worker started, waiting for jobs...")
         worker.work()
