593fa826dff86b71bd2fa78c32b2e22a55854bf2
Hanan Bahtiti <55956102+hananbahtiti@users.noreply.github.com>
2025-06-16 13:52:43 +0300

    Update tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -9,7 +9,7 @@ load_dotenv() #new
 #fal_client.configure(api_key=os.getenv("FAL_API_KEY")) #new
 
 import os
-os.environ["FAL_KEY"]   ="9bb92bb6-5489-40e4-92a5-93ba0d8b8011:19a4171e20d14538b6bf55639c18d0f5"
+os.environ["FAL_KEY"]   ="671b3239-6a7c-4449-a85e-caa161fe3351:631d5730d85692de182d398b1b072496"
 
 redis_conn = Redis(host="redis", port=6379)
 logging.basicConfig(level=logging.INFO)
