c6427326e45c0f8702291ce4184564303a5339a4
Hanan Bahtiti <55956102+hananbahtiti@users.noreply.github.com>
2025-06-21 10:34:14 +0300

    Update tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -6,10 +6,9 @@ import json
 from dotenv import load_dotenv #new
 load_dotenv() #new
 
-# Set the API key from environment variable
-#fal_client.configure(api_key=os.getenv("FAL_API_KEY")) #new
 
-import os
+
+
 os.environ["FAL_KEY"]   ="671b3239-6a7c-4449-a85e-caa161fe3351:631d5730d85692de182d398b1b072496"
 
 redis_conn = Redis(host="redis", port=6379)
@@ -17,12 +16,12 @@ logging.basicConfig(level=logging.INFO)
 
 RESULT_TTL = 3600
 
-async def generate_image(prompt, client_id):
+async def generate_image(model_name, prompt, client_id):
   """Generate an image based on the given prompt using fal-client"""
   try:
     logging.info(f"Generating image for client {client_id} ...")
     handler = fal_client.submit(
-      "fal-ai/flux-pro/v1.1",
+      model_name, #"fal-ai/flux-pro/v1.1"
       arguments={"prompt":prompt},
     )
     result = handler.get()
